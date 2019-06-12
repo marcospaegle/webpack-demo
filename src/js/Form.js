1e401f2062f9bc2c04488d0ff8331d2e2c549b3b
Marcos Paegle <marcospaegle@pop-os.localdomain>
2019-06-12 10:55:06 +0100

    first commit

---
+++ src/js/Form.js
@@ -0,0 +1,7 @@
+class Form {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+export default Form;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: (process.env.NODE_ENV === 'production') ? process.env.NODE_ENV : 'development',
+    entry: {
+        app: [
+            './src/js/app.js',
+            './src/scss/app.scss'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './public/assets/js'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '../images/[name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/[name].css'
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: (process.env.NODE_ENV === 'production') ? true : false
+        })
+    ]
+};
